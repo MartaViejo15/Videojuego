f613e1aeb2931003e5d90b86ec18760ed00035c9
Martu <martavgarcia15@hotmail.com>
2025-05-09 11:50:45 +0200

    Primer commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module es.uah.matcomp.teoria.gui.mvc.javafx.conquista {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens es.uah.matcomp.teoria.gui.mvc.javafx.conquista to javafx.fxml;
+    exports es.uah.matcomp.teoria.gui.mvc.javafx.conquista;
+}
