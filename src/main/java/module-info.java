dd6427c9636105d259737dfc340ad2147239c7f2
rober <robertrafael.rrsm@gmail.com>
2025-05-25 19:02:29 +0200

    log

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,6 +6,8 @@ module es.uah.matcomp.teoria.gui.mvc.javafx.conquista {
     requires com.dlsc.formsfx;
     requires org.kordamp.bootstrapfx.core;
     requires java.desktop;
+    requires com.google.gson;
+    requires org.apache.logging.log4j;
 
     opens es.uah.matcomp.teoria.gui.mvc.javafx.conquista to javafx.fxml;
     exports es.uah.matcomp.teoria.gui.mvc.javafx.conquista;
